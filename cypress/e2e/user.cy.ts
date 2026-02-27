1fa92bf6ec53f2f35ecda548ce63529fef607d84
Zach Kelling <z@zeekay.io>
2026-02-27 15:07:50 -0800

    rebrand cypress e2e tests: remove all umami references
    - Rename env vars umami_user/password → admin_user/password
    - Update localStorage key to hanzo.analytics.auth
    - Rebrand docker-compose service and DB credentials
    - Align cypress config with app AUTH_TOKEN constant

--- cypress/e2e/user.cy.ts
+++ cypress/e2e/user.cy.ts
@@ -2,7 +2,7 @@ describe('User tests', () => {
   Cypress.session.clearAllSavedSessions();
 
   beforeEach(() => {
-    cy.login(Cypress.env('umami_user'), Cypress.env('umami_password'));
+    cy.login(Cypress.env('admin_user'), Cypress.env('admin_password'));
     cy.visit('/settings/users');
   });
 
